0962a0a5d8666a318fb84aed7958ae27e8980bf8
Melissa <melissa.wanjiku@student.moringaschool.com>
2023-12-16 00:51:01 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+var num1 = 31
+var num2 = 2
+
+var multipy = num1 * num2
+
+
+var random = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
+
+var num3 = 14
+var num4 = 5
+
+var mod = num3 / num4
+
+console.log(mod)
+
+let numberSet = [13, 6, 20, 2]; 
+var max = Math.max(...numberSet)
+
+console.log(max)
